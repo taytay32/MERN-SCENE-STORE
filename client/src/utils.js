3435a161f215b13a43065fb7ff98400dfef9122b
Taylor Martin <taylorrobertmartin@gmail.com>
2021-04-19 13:45:58 -0400

    removed slash from API_URL

--- client/src/utils.js
+++ client/src/utils.js
@@ -4,5 +4,5 @@
 
 export const API_URL =
   process.env.NODE_ENV === "production"
-    ? "https://gurth-store-app.herokuapp.com/"
+    ? "https://gurth-store-app.herokuapp.com"
     : "http://localhost:5000";
